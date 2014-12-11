ea801016586a051c1d17dbf8bac6ce0bb3dd677a
HansSchollaardt93 <hansschollaardt@hotmail.com>
2014-12-11 13:49:59 +0100

    shitloads

--- Week2/src/Pete.java
+++ Week2/src/Pete.java
@@ -2,7 +2,8 @@ public abstract class Pete extends Thread {
 	protected String name;
 	private static int referenceId = 100;
 	protected int id;
-
+	protected int r,g,b;
+	
 	public Pete(String name) {
 		super(name);
 		this.name = name;
@@ -17,21 +18,22 @@ public abstract class Pete extends Thread {
 	 * Call this method to simulate a working state of the Pete
 	 * 
 	 * @param maxSeconds
-	 *            how long to work maximal in seconds
+	 *            how long to work at maximum in seconds
 	 */
 	public void doWork(int maxSeconds) {
 		try {
-			int sleep = (int) (Math.random() * (maxSeconds * 1000));
-			System.out.println(this + " starting to work for maximal " + sleep);
+			int sleep = (int) (Math.random() * (maxSeconds * 1000)+5);
+			System.out.println(this + " has started to work for " + (sleep/1000) + " seconds");
 			Thread.sleep(sleep);
-			System.out.println(this + " done working for: " + sleep);
+			System.out.println(this + " is done working");
 		} catch (InterruptedException e) {
 			e.printStackTrace();
+			System.err.println("Pete's work has been interrupted!");
 		}
 	}
 
 	@Override
 	public String toString() {
-		return getClass().getName() + " ID: " + id + " with name: " + name;
+		return  "Pete with name: " + name;
 	}
 }
--- Week2/src/SaintsMansion.java
+++ Week2/src/SaintsMansion.java
@@ -4,15 +4,27 @@ public class SaintsMansion {
 	private static final int NR_OF_GATHERPETES = 10;
 	private static final int NR_OF_WORKPETES = 3;
 
+	private static final int WORKPETES_NEEDED_GATHERING = 1;
+	private static final int GATHERPETES_NEEDED_GATHERING = 3;
+
+	private int availableWorkPetes = 0;
+	private int availableGatherPetes = 0;
+
 	private Thread[] gatherpetes;
 	private Thread[] workerpetes;
 
-	private Semaphore gatherPeteMeeting, workPeteMeeting, workMutex,
+	public Semaphore gatherPeteMeeting, workPeteMeeting, workMutex,
 			gatherMutex;
 
 	public SaintsMansion() {
 		gatherpetes = new Thread[NR_OF_GATHERPETES];
 		workerpetes = new Thread[NR_OF_WORKPETES];
+
+		// initially no pete's to wait for
+		gatherPeteMeeting = new Semaphore(0, true);
+		workPeteMeeting = new Semaphore(0, true);
+		workMutex = new Semaphore(1, true);
+		gatherMutex = new Semaphore(1, true);
 
 		createPeteThreads();
 
@@ -32,4 +44,153 @@ public class SaintsMansion {
 		}
 
 	}
+
+	/*
+	 * 
+	 * SAINT
+	 */
+	class Saint extends Thread {
+		public boolean inMeeting = false;
+
+		@Override
+		public void run() {
+			while (true) {
+				regenerateEnergy();
+				attendGathering();
+				try {
+					if (availableGatherPetes == WORKPETES_NEEDED_GATHERING
+							&& availableWorkPetes == GATHERPETES_NEEDED_GATHERING) {
+
+						System.err.println("<---- START A MEETING!! ---->");
+
+						System.err.println("<---- END OF MEETING!! ---->");
+					} else {
+						// sleep again
+
+					}
+
+					workPeteMeeting.acquire();
+
+				} catch (InterruptedException e) {
+				}
+
+			}
+		}
+
+		private void regenerateEnergy() {
+			try {
+				System.out
+						.println("The Saint is tired, and rests for a short while");
+				Thread.sleep((int) (Math.random() * 10000));
+				System.out
+						.println("The Saint has woken up, disoriented looking if any meeting is necessary...");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				System.err.println("Uh-oh, Saints rest has been interrupted!");
+			}
+		}
+
+		/**
+		 * Verzameloverleg: Minimaal 3 werkpieten, minimaal 1 verzamelpiet
+		 * beschikbaar Werkoverleg: Minimaal 3 werkpieten, GEEN verzamelpiet
+		 * beschikbaar
+		 */
+		private void attendGathering() {
+
+			if (canAttendGatherMeeting()) {
+				// Go have a GatherMeeting
+				attentGatherMeeting();
+			} else if(canAttendWorkMeeting()){
+				// Go have a WorkMeeting
+				attendWorkMeeting();
+			}
+		}
+
+		/**
+		 * 
+		 */
+		private void attendWorkMeeting() {
+			// TODO Auto-generated method stub
+
+		}
+
+		/**
+		 * 
+		 */
+		private void attentGatherMeeting() {
+			// TODO Auto-generated method stub
+
+		}
+
+		private boolean canAttendGatherMeeting() {
+
+			return (availableGatherPetes >= GATHERPETES_NEEDED_GATHERING);
+		}
+
+		private boolean canAttendWorkMeeting() {
+
+			return (availableWorkPetes >= WORKPETES_NEEDED_GATHERING);
+		}
+	}
+
+	/*
+	 * GATHERPETE
+	 */
+	class GatherPete extends Pete {
+		public GatherPete(String name) {
+			super(name);
+		}
+
+		@Override
+		public void run() {
+			while (true) {
+				doWork(25);
+				try {
+					gatherMutex.acquire();
+					// TODO Laten wachten ook al zijn er voldoende wachtende?
+					if (availableGatherPetes < GATHERPETES_NEEDED_GATHERING) {
+						availableGatherPetes++;
+						gatherMutex.release();
+					} else {
+						gatherMutex.release();
+					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	/*
+	 * WORKPETE
+	 */
+	class WorkPete extends Pete {
+
+		public WorkPete(String name) {
+			super(name);
+		}
+
+		@Override
+		public void run() {
+			while (true) {
+				doWork(10);
+				// Go to the Saint; check for meeting
+				try {
+					workMutex.acquire();
+					// TODO Laten wachten ook al zijn er voldoende wachtende?
+					if (availableWorkPetes < WORKPETES_NEEDED_GATHERING) {
+						availableWorkPetes++;
+						workMutex.release();
+					} else {
+						workMutex.release();
+					}
+
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+			}
+		}
+	}
+
 }
